f50cccd4a3f823b3a171a98504bc2be3a9ca9e52
Ezio <133083000+DaniAuditore@users.noreply.github.com>
2024-10-15 13:47:37 -0300

    feat: añadir componente ButtonsApi

---
+++ src/components/ButtonsApi.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="button-style">
+    <button @click="buttonPress" class="button-details">{{ label }}</button>
+</div>
+</template>
+
+<script>
+
+export default {
+    methods: {
+        buttonPress: function() {
+                console.log(apis[0]);
+        }
+    }
+};
+let apis = 
+["https://countriesnow.space/api/v0.1/countries/population",
+ "https://api.jikan.moe/v4/recommendations/anime",
+ "https://api.jikan.moe/v4/random/anime",
+ "https://api.jikan.moe/v4/random/manga"]
+
+</script>
+
+<style>
+    .button-style {
+        height: 75%;
+        width: 75%;
+        background-color: rgb(3, 102, 38);
+        padding: auto;
+        color: rgb(0, 255, 162);
+    }
+
+    .button-details {
+        background-color: rgb(0, 255, 162);
+        color: rgb(0, 0, 0);
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+</style>
--- src/components/PrincipalMenu.vue
+++ src/components/PrincipalMenu.vue
@@ -1,15 +1,37 @@
 <template>
-    <div>
-        <p>General information about the app</p>
+    <div class="principal-text">
+        <p>Presione un botón para acceder a una API</p>
     </div>
+
+    <div class="menu">
+        <ButtonsApi label="asadasd" />
+        <ButtonsApi label="a" />
+        <ButtonsApi label="a" />
+        <ButtonsApi label="a" />
+
+    </div>
+
 </template>
 
 <script>
 
+import ButtonsApi from './ButtonsApi.vue';
+
+export default {
+    name: 'PrincipalMenu',
+    components: {
+        ButtonsApi
+    }
+}
+
 </script>
 
 <style>
-h1 {
-    color: blue;
-}
+    .principal-text {
+        height: 75%;
+        width: 75%;
+        background-color: black;
+        padding: auto;
+        color: rgb(0, 255, 162);
+    }
 </style>
